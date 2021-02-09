af82cc1efe066b1823be94b7406d3c56b1f3e6be
unknown <timoncr9@gmail.com>
2021-02-09 10:47:11 +0200

    Data written to es from dynamodb stream

--- backend/src/lambda/http/writeDataToES.ts
+++ backend/src/lambda/http/writeDataToES.ts
@@ -1,33 +1,30 @@
-// import * as elasticsearch from 'elasticsearch'
-// import * as AWSES from 'http-aws-es'
+import * as elasticsearch from 'elasticsearch'
 
-// const client = new elasticsearch.Client({
-//     host: "https://udacity-serverless:Htmko1206-1965@search-timon-dev-es-sn23hvbrylsbmnrd2uitjvxfam.us-east-2.es.amazonaws.com",
-//     connectionClass: AWSES,
-//     log: "error"
-// })
+const client = new elasticsearch.Client({
+    host: "https://username:password@search-timon-dev-ztcdorjuka4lwb75sz72h4qoly.us-east-2.es.amazonaws.com",
+    log: "error"
+})
 
 exports.handler = function(event/*, callback*/) {
     console.log(JSON.stringify(event, null, 2));
-    // event.Records.forEach(function(record) {
-    //     try{
-    //         if(record.eventName === "INSERT"){
-    //             let result = client.create({
-    //                 index: 'description',
-    //                 type: 'todos',
-    //                 id: record.dynamodb.NewImage.todoId.S,
-    //                 body: {
-    //                     id: record.dynamodb.NewImage.todoId.S,
-    //                     name: record.dynamodb.NewImage.name.S,
-    //                     description: record.dynamodb.NewImage.description.S
-    //                 }
-    //             })
-    //             console.log(result)
-    //         }
-    //     }
-    //     catch(err){
-    //         console.log(err);
-    //     }
-    // });
-    // callback(null, "message");
+    event.Records.forEach(function(record) {
+        try{
+            if(record.eventName === "INSERT"){
+                let result = client.create({
+                    index: 'description',
+                    type: 'todos',
+                    id: record.dynamodb.NewImage.todoId.S,
+                    body: {
+                        id: record.dynamodb.NewImage.todoId.S,
+                        name: record.dynamodb.NewImage.name.S,
+                        description: record.dynamodb.NewImage.description.S
+                    }
+                })
+                console.log(result)
+            }
+        }
+        catch(err){
+            console.log(err);
+        }
+    });
 };
